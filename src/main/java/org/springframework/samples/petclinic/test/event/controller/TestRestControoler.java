8f54ddbe25e6f51ccfeb09b181b4b656fb894c42
hamhosik <hhs9102@naver.com>
2019-08-08 23:17:28 +0900

    부하테스트 매핑추가

---
+++ src/main/java/org/springframework/samples/petclinic/test/event/controller/TestRestControoler.java
@@ -0,0 +1,39 @@
+package org.springframework.samples.petclinic.test.event.controller;
+
+
+import net.minidev.json.JSONObject;
+import org.springframework.samples.petclinic.owner.Owner;
+import org.springframework.samples.petclinic.owner.OwnerRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class TestRestControoler {
+
+    OwnerRepository ownerRepository;
+
+    public TestRestControoler(OwnerRepository ownerRepository) {
+        this.ownerRepository = ownerRepository;
+    }
+
+    @GetMapping("overheadtest")
+    public @ResponseBody  JSONObject overheadtest(){
+        Map<String, String> dog = new HashMap<>();
+        dog.put("age", "3");
+        dog.put("name", "또또");
+        dog.put("addres","중구");
+        dog.put("owner","함호식");
+        JSONObject jsonObject = new JSONObject();
+        for(Map.Entry<String, String> entry : dog.entrySet()){
+            jsonObject.put(entry.getKey(), entry.getValue());
+        }
+        List<Owner> ownerList = (List<Owner>) ownerRepository.findByLastName("");
+        System.out.println(ownerList);
+        return jsonObject;
+    }
+}
